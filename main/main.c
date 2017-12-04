ece1485883aa0aaac5fa3005abb0acc4e041308a
Ryan McLean <pvtryan100@gmail.com>
2017-12-04 22:30:23 +0000

    Added includes & updated menuconfig

--- main/main.c
+++ main/main.c
@@ -1,8 +1,27 @@
+#include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include "nvs.h"
+#include "nvs_flash.h"
+#include "controller.h"
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/event_groups.h"
+#include "freertos/task.h"
 
+#include "esp_wifi.h"
+#include "esp_event_loop.h"
+#include "lwip/sockets.h"
+#include "lwip/dns.h"
+#include "lwip/netdb.h"
 
-
-
+#include "bt.h"
+#include "esp_gap_ble_api.h"
+#include "esp_gattc_api.h"
+#include "esp_gatt_defs.h"
+#include "esp_bt_main.h"
+//#include "esp_gatt_common_api.h" // Doesnt exist in 2.1.1
 
 void app_main() {
 
